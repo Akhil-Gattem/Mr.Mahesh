617a2691fc73a62ffe26e6d3f80fbbb0d5f8c051
akhil.gattem <akhil.gattem@rampgroup.com>
2021-06-16 17:58:12 +0530

    16/6/21

--- app/src/main/java/com/example/maheshbabu/ui/home/InfoScreenActivity.kt
+++ app/src/main/java/com/example/maheshbabu/ui/home/InfoScreenActivity.kt
@@ -3,15 +3,13 @@ package com.example.maheshbabu.ui.home
 import android.animation.AnimatorInflater
 import android.animation.AnimatorSet
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
 import com.bumptech.glide.Glide
 import com.example.maheshbabu.R
-import com.example.maheshbabu.StartScreenActivity
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.layout_info_screen.*
+
 
 class InfoScreenActivity : AppCompatActivity() {
     private var titleData = ""
@@ -21,7 +19,7 @@ class InfoScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_info_screen)
         info_back_btn.setOnClickListener {
-            animation(info_back_btn,R.animator.back_btn_animator)
+            animation(info_back_btn, R.animator.back_btn_animator)
             onBackPressed()
         }
         val extras = intent.extras
@@ -31,12 +29,28 @@ class InfoScreenActivity : AppCompatActivity() {
         }
         Glide.with(info_poster).load(posterData).error(R.color.light_dark).into(info_poster)
         info_title.text = titleData
+        info_title.isSelected = true
+        getMovieSummary()
     }
 
     private fun animation(imageView: View, animator: Int) {
         val set = AnimatorInflater.loadAnimator(applicationContext, animator) as AnimatorSet
         set.setTarget(imageView)
         set.start()
+    }
+
+    private fun getMovieSummary() {
+        val databaseReference = FirebaseDatabase.getInstance().reference
+        val query = databaseReference.child("summary").child("one")
+        query.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (data in dataSnapshot.children) {
+                    val summaryText = data.value.toString()
+                    summary.text = summaryText
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
     }
 
 }
